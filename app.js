b805298479b74376c3582234e421830c0ad830fa
OsamaTalal <169051670+OsamaTalal@users.noreply.github.com>
2024-07-01 01:24:41 +0300

    Update2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3001;
 const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
-const Mydata = require("./models/mydataSchema");
+const User = require("./models/customerSchema");
 app.set("view engine", "ejs");
 app.use(express.static('public'))
 
@@ -13,11 +13,11 @@ const path = require("path");
 const livereload = require("livereload");
 const liveReloadServer = livereload.createServer();
 liveReloadServer.watch(path.join(__dirname, 'public'));
- 
- 
+
+
 const connectLivereload = require("connect-livereload");
 app.use(connectLivereload());
- 
+
 liveReloadServer.server.once("connection", () => {
   setTimeout(() => {
     liveReloadServer.refresh("/");
@@ -28,11 +28,11 @@ liveReloadServer.server.once("connection", () => {
 
 
 
-
+//GET REQUEST
 
 app.get("/", (req, res) => {
-// result ==> array of objects
-res.render("index", {   });
+  // result ==> array of objects
+  res.render("index", {});
 
 });
 
@@ -48,9 +48,30 @@ app.get("/user/edit.html", (req, res) => {
   res.render("user/edit")
 });
 
+
+//POST REQUEST
+
+
+app.post("/user/add.html", (req, res) => {
+  console.log(req.body)
+  res.redirect("/user/add.html")
+  const user = new User(req.body);
+  user.save()
+  .then(() => {
+
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+});
+
+
+
+
+
 mongoose
   .connect(
-    "mongodb+srv://devali:Y3mkQvum8gItJ861@cluster0.5cqribi.mongodb.net/all-data?retryWrites=true&w=majority"
+    "mongodb+srv://osamatalal744:o123@cluster0.d2wvcro.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
     app.listen(port, () => {
